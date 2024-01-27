3fd93fa37705cb94db232c42a5f8225efb04e98b
Luan Aquino de Oliveira <luan.aq.ol@gmail.com>
2024-01-26 22:32:11 -0300

    instalado  sonarqube e sonar-scanner

--- sonarqube-lab/provision.sh
+++ sonarqube-lab/provision.sh
@@ -2,9 +2,9 @@
 
 useradd sonar
 yum install wget unzip java-11-openjdk-devel -y
-wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.3.0.82913.zip
-unzip sonarqube-10.3.0.82913.zip -d /opt/
-mv /opt/sonarqube-10.3.0.82913 /opt/sonarqube
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.6.1.59531.zip
+unzip sonarqube-9.6.1.59531.zip -d /opt/
+mv /opt/sonarqube-9.6.1.59531 /opt/sonarqube
 chown -R sonar:sonar /opt/sonarqube
 touch /etc/systemd/system/sonar.service
 echo > /etc/systemd/system/sonar.service
@@ -23,3 +23,13 @@ Restart=always
 WantedBy=multi-user.target
 EOT
 service sonar start
+
+#instalar sonar scaner
+
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-5.0.1.3006-linux.zip
+unzip sonar-scanner-cli-5.0.1.3006-linux.zip -d /opt/
+mv /opt/sonar-scanner-cli-5.0.1.3006-linux /opt/sonar-scanner
+chown -R sonar:sonar /opt/sonar-scanner
+echo 'export PATH=$PATH:/opt/sonar-scanner/bin' | sudo tee -a /etc/profile
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+sudo yum install nodejs -y
